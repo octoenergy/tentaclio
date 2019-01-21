023617dc73b5eb5b85e5f3c68977207ee11410cc
Igor Gotlibovych <igor.gotlibovych@gmail.com>
2019-01-21 17:10:47 +0000

    Add support for key-value pairs.

--- src/dataio/cli/url.py
+++ src/dataio/cli/url.py
@@ -21,14 +21,23 @@ def _compose_url(scheme, username, password, hostname, port, path, query):
 @click.option('--username', default=None)
 @click.option('--password', default=None)
 @click.option('--hostname', default=None)
-@click.option('--port', default=None)
+@click.option('--port', default=None, type=int)
 @click.option('--path', default=None)
-@click.option('--query', default=None)
-def command_compose_url(scheme, username, password, hostname, port, path, query):
+@click.option('--key', default=None, nargs=2, multiple=True,
+    help='Provide key-value pairs using [--key KEY VALUE]'
+)
+def command_compose_url(scheme, username, password, hostname, port, path, key):
+    if not hostname and not path:
+        raise click.ClickException('Provide at least one of `hostname`, `path`.')
+    query = key
+    if query:
+        query = {k: v for k, v in key}
+    else:
+        query = None
     url = _compose_url(
         scheme=scheme,
         username=username,
-        password=username,
+        password=password,
         hostname=hostname,
         port=port,
         path=path,
