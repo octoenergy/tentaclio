bb4d01e1b6460058f14b048e45db31b2ae47a705
pieeri <eric@rubicubix.co.uk>
2019-01-22 17:51:45 +0000

    Add base and s3 buffers to read and write from url

---
+++ tests/buffers/test_s3.py
@@ -0,0 +1,48 @@
+import boto3
+import moto
+import pytest
+from dataio.buffers import s3_buffer
+
+AWS_PUBLIC_KEY = "public_key"
+AWS_PRIVATE_KEY = "private_key"
+
+
+@pytest.fixture()
+def fixture_conn():
+    with moto.mock_s3():
+        yield
+
+
+class TestS3Buffer:
+    @pytest.mark.parametrize(
+        "url,bucket,key",
+        [(f"s3://{AWS_PUBLIC_KEY}:{AWS_PRIVATE_KEY}@s3", "test-bucket", "test.key")],
+    )
+    def test_reading_key(self, url, bucket, key, fixture_conn):
+        stream = "tested stream"
+        conn = boto3.session.Session(
+            aws_access_key_id=AWS_PUBLIC_KEY, aws_secret_access_key=AWS_PRIVATE_KEY
+        ).client("s3")
+        conn.create_bucket(Bucket=bucket)
+        conn.put_object(Bucket=bucket, Key=key, Body=stream)
+
+        with s3_buffer.open_s3_reader(url, bucket_name=bucket, key_name=key) as reader:
+            assert reader.read().decode() == stream
+
+    @pytest.mark.parametrize(
+        "url,bucket,key",
+        [(f"s3://{AWS_PUBLIC_KEY}:{AWS_PRIVATE_KEY}@s3", "test-bucket", "test.key")],
+    )
+    @moto.mock_s3
+    def test_writing_and_reading_key(self, url, bucket, key, fixture_conn):
+        stream = "tested stream"
+        conn = boto3.session.Session(
+            aws_access_key_id=AWS_PUBLIC_KEY, aws_secret_access_key=AWS_PRIVATE_KEY
+        ).client("s3")
+        conn.create_bucket(Bucket=bucket)
+
+        with s3_buffer.open_s3_writer(url, bucket_name=bucket, key_name=key) as writer:
+            writer.write(stream.encode())
+
+        with s3_buffer.open_s3_reader(url, bucket_name=bucket, key_name=key) as reader:
+            assert reader.read().decode() == stream
