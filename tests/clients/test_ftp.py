99e7bc46fafe9331236cfbfba0fe74a4566ee3ab
pieeri <eric@rubicubix.co.uk>
2019-01-22 11:17:45 +0000

    Extend ftp client unit tests

--- tests/clients/test_ftp.py
+++ tests/clients/test_ftp.py
@@ -2,10 +2,20 @@ import pytest
 from dataio.clients import exceptions, ftp_client
 
 
+@pytest.fixture()
+def mocked_ftp_conn(mocker):
+    with mocker.patch.object(ftp_client.FTPClient, "get_conn", return_value=mocker.Mock()):
+        yield
+
+
+@pytest.fixture()
+def mocked_sftp_conn(mocker):
+    with mocker.patch.object(ftp_client.SFTPClient, "get_conn", return_value=mocker.Mock()):
+        yield
+
+
 class TestFTPClient:
-    @pytest.mark.parametrize(
-        "url", ["file:///test.file", "sftp://:@localhost", "s3://:@s3"]
-    )
+    @pytest.mark.parametrize("url", ["file:///test.file", "sftp://:@localhost", "s3://:@s3"])
     def test_invalid_scheme(self, url):
         with pytest.raises(exceptions.FTPError):
             ftp_client.FTPClient(url)
@@ -19,7 +29,7 @@ class TestFTPClient:
             ("ftp://:@localhost:21/path", "", "", "localhost", 21, "/path"),
         ],
     )
-    def test_ftp_url(self, url, username, password, hostname, port, path):
+    def test_parsing_ftp_url(self, url, username, password, hostname, port, path):
         parsed_url = ftp_client.FTPClient(url).url
 
         assert parsed_url.scheme == "ftp"
@@ -29,11 +39,23 @@ class TestFTPClient:
         assert parsed_url.port == port
         assert parsed_url.path == path
 
+    @pytest.mark.parametrize("url,path", [("ftp://:@localhost", None)])
+    def test_get_invalid_path(self, url, path, mocked_ftp_conn):
+        with ftp_client.FTPClient(url) as client:
+
+            with pytest.raises(exceptions.FTPError):
+                client.get(file_path=path)
+
 
 class TestSFTPClient:
-    @pytest.mark.parametrize(
-        "url", ["file:///test.file", "ftp://:@localhost", "s3://:@s3"]
-    )
+    @pytest.mark.parametrize("url", ["file:///test.file", "ftp://:@localhost", "s3://:@s3"])
     def test_invalid_scheme(self, url):
         with pytest.raises(exceptions.FTPError):
             ftp_client.SFTPClient(url)
+
+    @pytest.mark.parametrize("url,path", [("sftp://:@localhost", None)])
+    def test_get_invalid_path(self, url, path, mocked_sftp_conn):
+        with ftp_client.SFTPClient(url) as client:
+
+            with pytest.raises(exceptions.FTPError):
+                client.get(file_path=path)
