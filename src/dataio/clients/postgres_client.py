a983ded5a9bb8411b5a4f09ddb6ab10fea2b1b07
javi <capitan.cambio@gmail.com>
2019-01-28 09:29:12 +0000

    Rename internal csv dump to reflect copy expert

--- src/dataio/clients/postgres_client.py
+++ src/dataio/clients/postgres_client.py
@@ -3,7 +3,7 @@ import io
 from typing import Generator, List, Optional
 
 import pandas as pd
-from dataio import Reader
+from dataio.protocols import Reader
 from sqlalchemy.engine import Connection, create_engine, result
 from sqlalchemy.engine import url as sqla_url
 from sqlalchemy.orm import session, sessionmaker
@@ -109,17 +109,19 @@ class PostgresClient(base_client.QueryClient):
 
     @decorators.check_conn()
     def dump_df(self, df: pd.DataFrame, dest_table: str) -> None:
+        """Dump a data frame into an existing Postgres table."""
         buff = io.StringIO()
         df.to_csv(buff, index=False)
         buff.seek(0)
-        self._dump_csv(buff, df.columns, dest_table)
+        self._copy_expert_csv(buff, df.columns, dest_table)
 
     @decorators.check_conn()
     def dump_csv(self, csv_reader: Reader, columns: List[str], dest_table: str) -> None:
-        self._dump_csv(csv_reader, columns, dest_table)
+        """Dump a csv reader into the database."""
+        self._copy_expert_csv(csv_reader, columns, dest_table)
 
-    def _dump_csv(self, csv_reader: Reader, columns: List[str], dest_table: str) -> None:
-        """ dumps a csv reader into the given table """
+    def _copy_expert_csv(self, csv_reader: Reader, columns: List[str], dest_table: str) -> None:
+        """Dump a csv reader into the given table. """
         sql_columns = ",".join(columns)
         sql_query = f"""COPY {dest_table} ({sql_columns}) FROM STDIN
                         WITH CSV HEADER DELIMITER AS ','
