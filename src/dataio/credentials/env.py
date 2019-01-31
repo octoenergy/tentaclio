0d8d70af7465ece461c922d6b0fae3f63b6b4b3c
javi <capitan.cambio@gmail.com>
2019-01-31 18:08:24 +0000

    Add automagically load credentials from env
    client apps/libraries can define OCTOIO_CONN_* env variables and the
    credentials defined in that url (i.e.
    ftp://green_tentacle:123sd@octopus.energy) will be injected when urls
    such as ftp://octopus.energy/path/to/my/file.txt are built

---
+++ src/dataio/credentials/env.py
@@ -0,0 +1,44 @@
+import logging
+import os
+from typing import Dict, List, Optional, cast
+
+from dataio import urls
+
+from .injection import CredentialsInjector
+
+
+logger = logging.getLogger(__name__)
+
+
+Env = Dict[str, str]
+
+OCTOIO_CONN_PREFIX = "OCTOIO_CONN"
+
+
+def _get_connection_urls(env: Env) -> List[urls.URL]:
+    connections = []
+    for key, val in env.items():
+        if key.startswith(OCTOIO_CONN_PREFIX):
+            try:
+                connections.append(urls.URL(val))
+            except Exception as e:
+                logger.error("Error parsing connection url {key}")
+                logger.exception(e)
+    return connections
+
+
+def add_creds_from_env(
+    injector: CredentialsInjector, env: Optional[Env] = None
+) -> CredentialsInjector:
+    """Add urls with credentials from the environment.
+
+    this funciton will scan the environment and add the variables
+    with the correct prefix to the pool of
+    available connection strings.
+    """
+    if env is None:
+        env = cast(Env, os.environ)
+    urls = _get_connection_urls(env)
+    for url in urls:
+        injector.register_credentials(url)
+    return injector
