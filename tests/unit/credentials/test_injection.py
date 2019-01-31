1bc01441f2c711c8c68232bf316047e5dd186875
javi <capitan.cambio@gmail.com>
2019-01-31 15:45:29 +0000

    Add credentials injection utility
    Prior to this change the user had to provide always username and
    password in the url itself for opening connections and streams.
    
    This change injects username and password if such credentials have been
    provided so the user doesn't have to worry about that.

---
+++ tests/unit/credentials/test_injection.py
@@ -0,0 +1,49 @@
+import pytest
+
+from dataio import URL
+from dataio.credentials import injection
+
+
+@pytest.mark.parametrize(
+    ["with_creds", "raw", "expected"],
+    [
+        [
+            "registered://user:password@octo.energy/path",
+            "registered://octo.energy/path",
+            "registered://user:password@octo.energy/path",
+        ],
+        [
+            "registered://user:password@octo.energy/path/",
+            "registered://octo.energy/path/with/more/elements",
+            "registered://user:password@octo.energy/path/with/more/elements",
+        ],
+        [
+            "registered://user:password@octo.energy/path/",
+            "registered://octo.energy/path/with/more/elements",
+            "registered://user:password@octo.energy/path/with/more/elements",
+        ],
+        [
+            "registered://user:password@octo.energy/database",
+            "registered://octo.energy/database::table",
+            "registered://user:password@octo.energy/database::table",
+        ],
+        [
+            "registered://user:password@octo.energy/database",
+            "registered://hostname/database",  # hostname wildcard
+            "registered://user:password@octo.energy/database",
+        ],
+        [
+            "registered://user:password@octo.energy/database",
+            "registered://octo.energy/database2",
+            "registered://octo.energy/database2",  # the path is similar but not identical
+        ],
+    ],
+)
+def test_simple_authenticate(with_creds, raw, expected, register_handler):
+    injector = injection.CredentialsInjector()
+    with_creds_url = URL(with_creds)
+    raw_url = URL(raw)
+    expected_url = URL(expected)
+    injector.register_credentials(with_creds_url)
+    result = injector.inject(raw_url)
+    assert expected_url == result
