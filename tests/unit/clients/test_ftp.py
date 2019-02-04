8fc4b535b209b271d4fca2d22f8bab7281b94ee5
javi <capitan.cambio@gmail.com>
2019-02-04 17:19:45 +0000

    Fix circleci
    Prior to this change,
    
    This change

--- tests/unit/clients/test_ftp.py
+++ tests/unit/clients/test_ftp.py
@@ -98,9 +98,8 @@ class TestSFTPClient:
         # the object from the fixture makes the mocking really ugly
         sftp_mock = mocker.patch("pysftp.Connection")
         result = io.BytesIO()
-        sftp_mock.return_value.open.return_value.__enter__.return_value.write = lambda data: result.write(
-            data
-        )
+        thing = sftp_mock.return_value.open.return_value
+        thing.__enter__.return_value.write = lambda data: result.write(data)
         with ftp_client.SFTPClient("sftp://user:pass@localhost/myfile.txt") as client:
             reader = io.BytesIO(expected)
             client.put(reader)
