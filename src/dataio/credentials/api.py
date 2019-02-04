0efd02eb77abb62b706d8a0cf9d071acd7242415
javi <capitan.cambio@gmail.com>
2019-02-04 13:54:26 +0000

    Have the api inject credentials to the urls directly
    Automatic credentials injection from the environment

---
+++ src/dataio/credentials/api.py
@@ -0,0 +1,16 @@
+import functools
+
+from .env import add_creds_from_env
+from .injection import CredentialsInjector
+
+
+__all__ = ["load_credentials_injector"]
+
+
+#  Singleton
+@functools.lru_cache(maxsize=1, typed=True)
+def load_credentials_injector() -> CredentialsInjector:
+    """Load the credentials injector fetching configuration from the environment."""
+    injector = CredentialsInjector()
+    injector = add_creds_from_env(injector)
+    return injector
