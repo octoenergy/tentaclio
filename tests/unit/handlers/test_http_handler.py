e4ac7db7baa37f8b3d5c6d32f7c2fd96aa4e3b2d
pieeri <eric@rubicubix.co.uk>
2019-02-05 17:58:33 +0000

    Disable HTTP writer

--- tests/unit/handlers/test_http_handler.py
+++ tests/unit/handlers/test_http_handler.py
@@ -3,15 +3,14 @@ import json
 import pytest
 import requests
 
-from dataio.api import open_reader
-from dataio.urls import URL
+from dataio import api, urls
 
 
 @pytest.mark.parametrize(
     "url,scheme", [("http://host.com/request", "http"), ("https://host.com/request", "https")]
 )
 def test_handler_is_registered(url, scheme):
-    assert URL(url).scheme == scheme
+    assert urls.URL(url).scheme == scheme
 
 
 def test_open_http_url_reading(mocker):
@@ -21,8 +20,16 @@ def test_open_http_url_reading(mocker):
         requests.Session, "send", return_value=mocked_response
     )
 
-    with open_reader("http://host.com/request") as reader:
+    with api.open_reader("http://host.com/request") as reader:
         content = reader.read()
 
     assert content == payload
     mocked_session_call.assert_called_once()
+
+
+def test_open_http_url_writer():
+    content = json.dumps([{"result": "nothing is alright"}])
+
+    with pytest.raises(NotImplementedError):
+        with api.open_writer("http://host.com/request") as writer:
+            writer.write(content)
