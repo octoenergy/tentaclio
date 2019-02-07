f2dca79bb41451328867763cade6bce8ae04bd95
pieeri <eric@rubicubix.co.uk>
2019-02-07 15:51:57 +0000

    Migrate Slack conn to package hooks

--- src/dataio/clients/__init__.py
+++ src/dataio/clients/__init__.py
@@ -2,5 +2,4 @@ from .ftp_client import *  # noqa
 from .http_client import *  # noqa
 from .postgres_client import *  # noqa
 from .s3_client import *  # noqa
-from .slack_client import *  # noqa
 from .stream_client import *  # noqa
