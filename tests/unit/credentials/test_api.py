f9ac008d4188dc484fd80b9d99a7f68b28c9dc71
javi <capitan.cambio@gmail.com>
2019-02-19 18:05:29 +0000

    Add shorthand for authenticating credentials

---
+++ tests/unit/credentials/test_api.py
@@ -0,0 +1,14 @@
+
+from dataio import urls
+from dataio import credentials
+
+
+def test_authenticate(mocker):
+
+    injector = credentials.CredentialsInjector()
+    injector.register_credentials(urls.URL("ftp://user:pass@google.com"))
+    mock_cred = mocker.patch("dataio.credentials.api.load_credentials_injector")
+    mock_cred.return_value = injector
+    authenticated = credentials.authenticate("ftp://google.com/myfile.csv")
+    assert authenticated.username == "user"
+    assert authenticated.password == "pass"
