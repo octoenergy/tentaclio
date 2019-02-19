39f2b028151ae61140eb8181f7feaff0e1395cb5
javi <capitan.cambio@gmail.com>
2019-02-19 18:05:29 +0000

    Add shorthand for authenticating credentials

--- src/dataio/credentials/api.py
+++ src/dataio/credentials/api.py
@@ -1,6 +1,8 @@
 import functools
 import logging
 
+from dataio import urls
+
 from .env import add_credentials_from_env
 from .injection import CredentialsInjector
 from .reader import add_credentials_from_env_file
@@ -9,7 +11,7 @@ from .reader import add_credentials_from_env_file
 logger = logging.getLogger(__name__)
 
 
-__all__ = ["load_credentials_injector"]
+__all__ = ["load_credentials_injector", "authenticate"]
 
 
 #  Singleton
@@ -20,3 +22,8 @@ def load_credentials_injector() -> CredentialsInjector:
     injector = add_credentials_from_env(injector)
     injector = add_credentials_from_env_file(injector)
     return injector
+
+
+def authenticate(url: str) -> urls.URL:
+    """Authenticate url."""
+    return load_credentials_injector().inject(urls.URL(url))
---
+++ tests/unit/credentials/test_api.py
@@ -0,0 +1,12 @@
+from dataio import credentials, urls
+
+
+def test_authenticate(mocker):
+
+    injector = credentials.CredentialsInjector()
+    injector.register_credentials(urls.URL("ftp://user:pass@google.com"))
+    mock_cred = mocker.patch("dataio.credentials.api.load_credentials_injector")
+    mock_cred.return_value = injector
+    authenticated = credentials.authenticate("ftp://google.com/myfile.csv")
+    assert authenticated.username == "user"
+    assert authenticated.password == "pass"
