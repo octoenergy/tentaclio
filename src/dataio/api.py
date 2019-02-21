b787a08d3a6de541dffcf26aa2ee2464c230af8d
pieeri <eric@rubicubix.co.uk>
2019-02-21 16:50:18 +0000

    Cleanup dataio api and inherent tests

--- src/dataio/api.py
+++ src/dataio/api.py
@@ -5,21 +5,11 @@ from dataio import credentials, protocols, urls
 
 __all__ = ["open"]
 
-
-def _open_reader(url: str, mode: str, **kwargs) -> ContextManager[protocols.Reader]:
-    """Opens the url and returns a reader """
-    authenticated = credentials.load_credentials_injector().inject(urls.URL(url))
-    return authenticated.open_reader(mode, extras=kwargs)
-
-
-def _open_writer(url: str, mode: str, **kwargs) -> ContextManager[protocols.Writer]:
-    """Opens the url and returns a writer"""
-    authenticated = credentials.load_credentials_injector().inject(urls.URL(url))
-    return authenticated.open_writer(mode, extras=kwargs)
+VALID_MODES = ("", "rb", "wb", "rt", "wt", "r", "w", "b", "t")
 
 
 def open(url: str, mode: str = None, **kwargs) -> ContextManager[protocols.AnyReaderWriter]:
-    """Opens the url and returns a reader or writer depending on mode
+    """Open a url and return a reader or writer depending on mode
 
     Examples:
         >>> open(path, 'b')  # opens binary reader
@@ -38,12 +28,23 @@ def open(url: str, mode: str = None, **kwargs) -> ContextManager[protocols.AnyRe
         return _open_reader(url=url, mode=mode, **kwargs)
 
 
-VALID_MODES = ("", "rb", "wb", "rt", "wt", "r", "w", "b", "t")
+# Helpers
 
 
 def _assert_mode(mode: str):
-    """Check that mode is valid or raise error otherwise.
-    """
+    """Check if a mode is valid or raise an error otherwise"""
     if mode not in VALID_MODES:
         valid_modes = ",".join(VALID_MODES)
         raise ValueError(f"Mode {mode} is not allowed. Valid modes are  {valid_modes}")
+
+
+def _open_writer(url: str, mode: str, **kwargs) -> ContextManager[protocols.Writer]:
+    """Open a url and return a writer"""
+    authenticated = credentials.load_credentials_injector().inject(urls.URL(url))
+    return authenticated.open_writer(mode, extras=kwargs)
+
+
+def _open_reader(url: str, mode: str, **kwargs) -> ContextManager[protocols.Reader]:
+    """Open a url and return a reader"""
+    authenticated = credentials.load_credentials_injector().inject(urls.URL(url))
+    return authenticated.open_reader(mode, extras=kwargs)
