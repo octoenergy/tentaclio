58a4737126c877bf9074c43d0016cec0b8de86c9
pieeri <eric@rubicubix.co.uk>
2019-02-21 18:03:20 +0000

    Remove empty buffer test directory

--- tests/functional/ftp/test_api.py
+++ tests/functional/ftp/test_api.py
@@ -1,13 +1,13 @@
-from dataio import api
+import dataio
 
 
 def test_authenticated_api_calls():
     data = bytes("Ph'nglui mglw'nafh Cthulhu R'lyeh wgah'nagl fhtagn", "utf-8")
 
-    with api.open("ftp://localhost/data.txt", mode="wb") as f:
+    with dataio.open("ftp://localhost/data.txt", mode="wb") as f:
         f.write(data)
 
-    with api.open("ftp://localhost/data.txt", mode="rb") as f:
+    with dataio.open("ftp://localhost/data.txt", mode="rb") as f:
         result = f.read()
 
     assert result == data
