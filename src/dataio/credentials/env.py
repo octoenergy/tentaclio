65b2ac0840ebb66a550753c3ecd1bc55516f7cb6
ali.teeney <alistair.teeney@octoenergy.com>
2019-02-26 10:03:26 -0600

    Correctly format error string

--- src/dataio/credentials/env.py
+++ src/dataio/credentials/env.py
@@ -22,7 +22,7 @@ def _get_connection_urls(env: Env) -> List[urls.URL]:
             try:
                 connections.append(urls.URL(val))
             except Exception as e:
-                logger.error("Error parsing credentials from the environment variable {key}")
+                logger.error(f"Error parsing credentials from the environment variable {key}")
                 raise e
     return connections
 
