925253927422e84223c1f3bd2ed5bb26d3fcd04b
javi <capitan.cambio@gmail.com>
2019-06-21 16:41:43 +0100

    Add listdir
    give support os.listdir like functionality to tentaclio

--- src/tentaclio/fs/api.py
+++ src/tentaclio/fs/api.py
@@ -6,7 +6,7 @@ from tentaclio import credentials
 from .scanner import SCANNER_REGISTRY, DirEntry
 
 
-__all__ = ["scandir"]
+__all__ = ["scandir", "listdir"]
 
 
 def scandir(url: str) -> Iterable[DirEntry]:
@@ -22,3 +22,18 @@ def scandir(url: str) -> Iterable[DirEntry]:
     """
     authenticated = credentials.authenticate(url)
     return SCANNER_REGISTRY.get_handler(authenticated.scheme).scandir(authenticated)
+
+
+def listdir(url: str) -> Iterable[str]:
+    """List a directory-like url returning its entries.
+
+    Return an iterator string containing the urls
+    to the entries in the directory given by url passed as parametre. The entries
+    are yielded in arbitrary order,
+    and the special entries '.' and '..' are not included.
+
+    More info:
+    https://docs.python.org/3/library/os.html#os.listdir
+    """
+    entries = scandir(url)
+    return (str(entry.url) for entry in entries)
