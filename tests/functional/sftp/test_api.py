cc933ec5fac28613ee325780ddd88c7a0e170aaa
javi <capitan.cambio@gmail.com>
2019-07-10 12:25:08 +0100

    Add functional tests for sftp scandir

--- tests/functional/sftp/test_api.py
+++ tests/functional/sftp/test_api.py
@@ -1,4 +1,6 @@
 import tentaclio
+import tentaclio.clients
+from tentaclio.credentials import authenticate
 
 
 def test_authenticated_api_calls():
@@ -11,3 +13,18 @@ def test_authenticated_api_calls():
         result = f.read()
 
     assert result == data
+
+
+def test_list_folders():
+    data = "Ph'nglui mglw'nafh Cthulhu R'lyeh wgah'nagl fhtagn"
+    cli = tentaclio.clients.SFTPClient(authenticate("sftp://hostname"))
+
+    with cli:
+        cli.conn.makedirs("upload/folder1/folder2")
+
+    with tentaclio.open("sftp://hostname/upload/folder1/data.txt", mode="w") as f:
+        f.write(data)
+
+    ls = list(tentaclio.listdir("sftp://hostname/upload/folder1"))
+    assert any("upload/folder1/data.txt" in entry for entry in ls)
+    assert any("upload/folder1/folder2" in entry for entry in ls)
