864d638e18ecaae11abf4d89adacd2f8213c9f60
javi <capitan.cambio@gmail.com>
2019-07-10 12:35:01 +0100

    Add functional test for ftp scandir

--- tests/functional/ftp/test_api.py
+++ tests/functional/ftp/test_api.py
@@ -1,4 +1,5 @@
 import tentaclio
+from tentaclio.credentials import authenticate
 
 
 def test_authenticated_api_calls():
@@ -11,3 +12,29 @@ def test_authenticated_api_calls():
         result = f.read()
 
     assert result == data
+
+
+def cdTree(ftp, currentDir):
+    # https://stackoverflow.com/a/18342179
+    if currentDir != "":
+        try:
+            ftp.cwd(currentDir)
+        except Exception:
+            cdTree(ftp, "/".join(currentDir.split("/")[:-1]))
+            ftp.mkd(currentDir)
+            ftp.cwd(currentDir)
+
+
+def test_list_folders():
+    data = "Ph'nglui mglw'nafh Cthulhu R'lyeh wgah'nagl fhtagn"
+    cli = tentaclio.clients.FTPClient(authenticate("ftp://hostname"))
+
+    with cli:
+        cdTree(cli.conn, "/upload/folder1/folder2")
+
+    with tentaclio.open("ftp://hostname/upload/folder1/data.txt", mode="w") as f:
+        f.write(data)
+
+    ls = list(tentaclio.listdir("ftp://hostname/upload/folder1"))
+    assert any("upload/folder1/data.txt" in entry for entry in ls)
+    assert any("upload/folder1/folder2" in entry for entry in ls)
