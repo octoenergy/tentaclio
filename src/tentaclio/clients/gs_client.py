77fe3fafe57797cf779b283d8fcec1969e4ea5b2
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2020-05-26 13:39:34 +0200

    GCS: GSClient init
    Initialisation of the GSClient implemented and unit tested.

---
+++ src/tentaclio/clients/gs_client.py
@@ -0,0 +1,61 @@
+"""GS Stream client."""
+from typing import Optional, Union
+
+from google.cloud import storage
+
+from tentaclio import urls
+
+from . import base_client
+
+
+__all__ = ["GSClient"]
+
+
+class GSClient(base_client.BaseClient["GSClient"]):
+    """GS client.
+
+    Ref: https://googleapis.dev/python/storage/latest/index.html
+    """
+
+    allowed_schemes = ["gs", "gcs"]
+
+    conn: storage.Client
+    bucket: Optional[str]
+    key_name: Optional[str]
+
+    def __init__(
+        self, url: Union[urls.URL, str]
+    ) -> None:
+        """Create a new GS client.
+
+        The client is created based on a URL of the form
+        gs://bucket/key.
+
+        Authentication and client configuration is recommended with environment variables.
+        Quoted from docs:
+            "Setting the GOOGLE_APPLICATION_CREDENTIALS and GOOGLE_CLOUD_PROJECT environment
+            variables will override the automatically configured credentials."
+
+        See documentation for more information.
+        https://googleapis.dev/python/google-api-core/latest/auth.html
+        """
+        super().__init__(url)
+
+        self.bucket = self.url.hostname or None
+        self.key_name = self.url.path[1:] if self.url.path != "" else ""
+
+        if self.bucket == "gs":
+            self.bucket = None
+
+    def _connect(self) -> storage.Client:
+        return storage.Client()
+
+    def close(self) -> None:
+        """Close the connection.
+
+        This is fake. GS doesn't allow to close gs connections.
+        """
+        # gs doesn't have close method
+        if self.closed:
+            raise ValueError("Trying to close a closed client")
+        self.closed = True
