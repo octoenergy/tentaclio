dc0c186b6b1c297a0b669144a9386b5ea6baf85f
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2020-05-26 14:52:52 +0200

    GCS: get implemented

--- src/tentaclio/clients/gs_client.py
+++ src/tentaclio/clients/gs_client.py
@@ -1,11 +1,11 @@
 """GS Stream client."""
-from typing import Optional, Union
+from typing import Optional, Union, Tuple, cast
 
 from google.cloud import storage
 
-from tentaclio import urls
+from tentaclio import urls, protocols
 
-from . import base_client
+from . import base_client, decorators, exceptions
 
 
 __all__ = ["GSClient"]
@@ -59,3 +59,54 @@ class GSClient(base_client.BaseClient["GSClient"]):
         if self.closed:
             raise ValueError("Trying to close a closed client")
         self.closed = True
+
+    # Stream methods:
+
+    @decorators.check_conn
+    def get(
+        self,
+        writer: protocols.ByteWriter,
+        bucket_name: Optional[str] = None,
+        key_name: Optional[str] = None
+    ) -> None:
+        """Download the contents from gs and write them in the provided writer.
+
+        Arguments:
+            :bucket_name: If not provided in the url at construction time.
+            :key_name: If not provided in the url at construction time.
+
+        Raises:
+            GSError: If a bucket or a key is not found in the given URL or args then
+            Google Cloud Exceptions: if the client raises them.
+
+        """
+        gs_bucket, gs_key = self._fetch_bucket_and_key(bucket_name, key_name)
+
+        self._get(writer, gs_bucket, gs_key)
+
+    # Helpers:
+
+    def _fetch_bucket_and_key(
+        self, bucket: Optional[str] = None, key: Optional[str] = None
+    ) -> Tuple[str, str]:
+        bucket_name = bucket or self.bucket
+        if bucket_name is None:
+            raise exceptions.GSError("Missing remote bucket")
+
+        key_name = key or self.key_name
+        if key_name == "":
+            raise exceptions.GSError("Missing remote key")
+
+        return bucket_name, cast(str, key_name)
+
+    def _get_bucket(self, bucket_name: str) -> storage.Bucket:
+        """Get the bucket client."""
+        return self.conn.bucket(bucket_name)
+
+    def _get(
+        self, writer: protocols.ByteWriter, bucket_name: str, key_name: str
+    ) -> None:
+        """Download on the client so we can mock it."""
+        bucket = self._get_bucket(bucket_name)
+        blob = bucket.blob(key_name)
+        blob.download_to_file(writer)
--- tests/unit/clients/test_gs_client.py
+++ tests/unit/clients/test_gs_client.py
@@ -1,9 +1,12 @@
 """Test of the GS Client."""
+import io
 
 import pytest
 import mock
 
-from tentaclio.clients import gs_client
+from google.cloud import exceptions as google_exceptions
+
+from tentaclio.clients import gs_client, exceptions
 
 
 @mock.patch("tentaclio.clients.GSClient._connect")
@@ -23,3 +26,73 @@ def test_parsing_gs_url(m_connect, url, hostname, path):
 
     assert client.key_name == path
     assert client.bucket == hostname
+
+
+@mock.patch("tentaclio.clients.GSClient._get")
+@mock.patch("tentaclio.clients.GSClient._connect")
+@pytest.mark.parametrize(
+    "url,bucket,key", [
+        ("gs://:@gs", None, None),
+        ("gs://:@gs", "bucket", None),
+        ("gs://:@bucket", None, None)
+    ],
+)
+def test_get_invalid_path(m_connect, m_get, url, bucket, key):
+    """Test get for with invalid paths."""
+    with gs_client.GSClient(url) as client:
+        with pytest.raises(exceptions.GSError):
+            client.get(io.StringIO(), bucket_name=bucket, key_name=key)
+
+    m_get.assert_not_called()
+
+
+@mock.patch("tentaclio.clients.GSClient._get")
+@mock.patch("tentaclio.clients.GSClient._connect")
+@pytest.mark.parametrize(
+    "url,bucket,key", [
+        ("gs://:@bucket/not_found", "bucket", "not_found")
+    ],
+)
+def test_get_not_found(m_connect, m_get, url, bucket, key):
+    """That when the connection raises a NotFound an GSError is thrown."""
+    m_get.side_effect = google_exceptions.NotFound("not found")
+    stream = io.StringIO()
+    with gs_client.GSClient(url) as client:
+        with pytest.raises(exceptions.GSError):
+            client.get(stream, bucket_name=bucket, key_name=key)
+
+    m_get.assert_called_once_with(stream, bucket, key)
+
+
+@mock.patch("tentaclio.clients.GSClient._get")
+@mock.patch("tentaclio.clients.GSClient._connect")
+@pytest.mark.parametrize(
+    "url,bucket,key", [
+        ("gs://bucket/prefix", "bucket", "prefix"),
+    ]
+)
+def test_get(m_connect, m_get, url, bucket, key):
+    """Test get valid."""
+    stream = io.StringIO()
+    with gs_client.GSClient(url) as client:
+        client.get(stream, bucket_name=bucket, key_name=key)
+
+    m_get.assert_called_once_with(stream, bucket, key)
+
+
+@mock.patch("tentaclio.clients.GSClient._connect")
+@pytest.mark.parametrize(
+    "url,bucket,key", [
+        ("gs://bucket/prefix", "bucket", "prefix"),
+    ]
+)
+def test_helper_get(m_connect, url, bucket, key):
+    """Test helper get is correctly called."""
+    stream = io.StringIO()
+    with gs_client.GSClient(url) as client:
+        client._get(stream, bucket_name=bucket, key_name=key)
+
+    connection = m_connect.return_value
+    connection.bucket.assert_called_once_with(bucket)
+    connection.bucket.return_value.blob.assert_called_once_with(key)
+    connection.bucket.return_value.blob.return_value.download_to_file.assert_called_once()
