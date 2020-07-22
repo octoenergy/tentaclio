b019433d4a8d81bdd7ca74c76f3139211e1d002c
javi <capitan.cambio@gmail.com>
2020-07-22 14:13:53 +0100

    Add token refreshing and loading logic
    The actual file to be loaded will be generated separately using the user
    input

--- src/tentaclio/clients/google_drive_client.py
+++ src/tentaclio/clients/google_drive_client.py
@@ -1,5 +1,11 @@
 """Local filesystem client."""
-from typing import Iterable, Tuple, Union
+import json
+import os
+from typing import Any, Iterable, Optional, Tuple, Union
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from googleapiclient.discovery import build
 
 from tentaclio import fs, protocols, urls
 
@@ -7,6 +13,33 @@ from . import base_client
 
 
 __all__ = ["GoogleDriveFSClient"]
+
+# Load the location of the token file from the environment
+TOKEN_FILE = os.getenv(
+    "TENTACLIO__GOOGLE_DRIVE_TOKEN_FILE", os.environ["HOME"] + os.sep + ".google_drive_token.json",
+)
+
+
+def _load_credentials(token_file: str) -> Credentials:
+    """Load the credentials and refresh them if necesary."""
+    creds = None
+    if os.path.exists(token_file):
+        with open(token_file) as f:
+            state = json.load(f)
+            creds = Credentials(**state)
+    else:
+        raise ValueError(f"Token file is not valid {token_file}")
+
+    # If there are no (valid) credentials available refresh them or raise an error.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            raise ValueError(f"Couldn't refresh token in f{token_file}")
+        # Save the credentials for the next run
+        with open(token_file, "w") as f:
+            f.write(creds.to_json())
+    return creds
 
 
 class GoogleDriveFSClient(base_client.BaseClient["GoogleDriveFSClient"]):
@@ -18,6 +51,10 @@ class GoogleDriveFSClient(base_client.BaseClient["GoogleDriveFSClient"]):
 
     drive: str
     parts: Tuple[str]
+
+    # Not an easy task to figure out the type of the
+    # returned value from the library
+    _service: Optional[Any] = None
 
     def __init__(self, url: Union[urls.URL, str]) -> None:
         """Create a new GoogleDriveFSClient."""
@@ -35,7 +72,15 @@ class GoogleDriveFSClient(base_client.BaseClient["GoogleDriveFSClient"]):
         self.path_parts = tuple(filter(lambda part: len(part) > 0, parts[1:]))
 
     def _connect(self) -> "GoogleDriveFSClient":
+        self._refresh_service()
         return self
+
+    def _refresh_service(self, token_file: str = TOKEN_FILE):
+        """Check the validity of the credentials."""
+        if self._service is not None:
+            return
+        creds = _load_credentials(token_file)
+        self._service = build("drive", "v3", credentials=creds)
 
     def close(self) -> None:
         """Close the dummy connection to google drive."""
--- tests/unit/clients/test_google_drive_client.py
+++ tests/unit/clients/test_google_drive_client.py
@@ -1,6 +1,52 @@
+import json
+import tempfile
+
 import pytest
 
 from tentaclio.clients import GoogleDriveFSClient
+from tentaclio.clients.google_drive_client import _load_credentials
+
+
+@pytest.fixture
+def token_file():
+    with tempfile.NamedTemporaryFile() as f:
+        with open(f.name, "w") as writer:
+            json.dump({"token": "toktok"}, writer)
+        yield f.name
+
+
+def test_load_credentials_bad_file():
+    with pytest.raises(ValueError, match="Token file is not valid"):
+        _load_credentials("not_a_valid_file")
+
+
+def test_load_credentials(mocker, token_file):
+    mocked_creds = mocker.patch("tentaclio.clients.google_drive_client.Credentials")
+    mocked_creds.return_value = mocker.MagicMock()
+    mocked_creds.return_value.valid = True
+    token = _load_credentials(token_file)
+    token.token == "toktok"
+
+
+def test_load_not_refreshing(mocker, token_file):
+    mocked_creds = mocker.patch("tentaclio.clients.google_drive_client.Credentials")
+    mocked_creds.return_value = mocker.MagicMock()
+    mocked_creds.return_value.valid = False
+    mocked_creds.return_value.refresh_token = False
+    with pytest.raises(ValueError, match="Couldn't refresh token"):
+        _load_credentials(token_file)
+
+
+def test_load_refreshing(mocker, token_file):
+    mocked_creds = mocker.patch("tentaclio.clients.google_drive_client.Credentials")
+    mocked_creds.return_value = mocker.MagicMock()
+    mocked_creds.return_value.valid = False
+    mocked_creds.return_value.refresh_token = "refresh"
+    mocked_creds.return_value.to_json.return_value = '{"token": "refreshed"}'
+    _load_credentials(token_file)
+    with open(token_file) as f:
+        creds = json.load(f)
+    assert creds == {"token": "refreshed"}
 
 
 class TestGoogleDriveFSClient:
