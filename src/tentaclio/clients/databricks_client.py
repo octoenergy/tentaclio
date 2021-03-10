9ebc2585931571da5f46fcfd42b3eda2493138c7
Jonathan Roberts <jonathan.roberts@octoenergy.com>
2021-03-10 18:20:57 +0000

    Get driver + http paths from env or url query

--- src/tentaclio/clients/databricks_client.py
+++ src/tentaclio/clients/databricks_client.py
@@ -1,24 +1,78 @@
+import logging
+import os
+import platform
 import urllib
+from typing import Union, Dict, Optional
 
 from sqlalchemy.engine import Connection, create_engine
 
+from tentaclio import urls
+
 from . import sqla_client
+
+
+DRIVER_URL_QUERY_KEY = "simba_driver"
+DRIVER_ENV_VAR_KEY = "TENTACLIO__SIMBA_DRIVER"
+
+HTTP_PATH_QUERY_KEY = "http_path"
+HTTP_ENV_VAR_KEY = "TENTACLIO__DATABRICKS_HTTP_PATH"
+
+
+DRIVER_DEFAULT_PATHS = {
+    "Windows": "path/to/simba/",
+    "Darwin": "/Library/simba/spark/lib/libsparkodbc_sbu.dylib",
+    "Linux": "/opt/simba/spark/lib/64/libsparkodbc_sb64.so",
+}
+
+
+logger = logging.getLogger(__name__)
 
 
 class DatabricksClient(sqla_client.SQLAlchemyClient):
     """Databricks client, backed by a pyodbc + SQLAlchemy connection"""
 
-    DRIVER = "/Library/simba/spark/lib/libsparkodbc_sbu.dylib"
+    def __init__(self, url: Union[str, urls.URL]) -> None:
+        super().__init__(url)
+        self.driver = self._get_driver_path()
+        self.http_path = self._get_http_path()
+
+    def _get_driver_path(
+        self,
+        env_var_key: str = DRIVER_ENV_VAR_KEY,
+        url_query_key: str = DRIVER_URL_QUERY_KEY,
+    ) -> str:
+        driver_path = get_param_from_url_query_or_env_var(
+            env_var_key, url_query_key, self.url.query
+        )
+        if driver_path == "":
+            platform_system = platform.system()
+            driver_path = DRIVER_DEFAULT_PATHS[platform_system]
+            logger.warning(
+                f"Using default driver path for {platform_system}: {driver_path}"
+            )
+        return driver_path
+
+    def _get_http_path(
+        self,
+        env_var_key: str = HTTP_ENV_VAR_KEY,
+        url_query_key: str = HTTP_PATH_QUERY_KEY,
+    ):
+        http_path = get_param_from_url_query_or_env_var(
+            env_var_key, url_query_key, self.url.query
+        )
+        if http_path == "":
+            raise KeyError("http_path required to connect to Databricks")
+        return http_path
 
     def _connect(self) -> Connection:
-        connection_url = _build_odbc_connection_string(
-            DRIVER=self.DRIVER,
+        connection_url = build_odbc_connection_string(
+            DRIVER=self.driver,
             HOST=self.host,
             PORT=self.port,
             UID=self.username,
             PWD=self.password,
             Schema=self.database,
-            HTTPPath=self.url.query["http_path"],
+            HTTPPath=self.http_path,
             AuthMech=3,
             SparkServerType=3,
             ThriftTransport=2,
@@ -36,3 +90,16 @@ class DatabricksClient(sqla_client.SQLAlchemyClient):
 def build_odbc_connection_string(**kwargs) -> str:
     connection_url = ";".join([f"{k}={v}" for k, v in kwargs.items()])
     return urllib.parse.quote(connection_url)
+
+
+def get_param_from_url_query_or_env_var(
+    env_var_key: str, url_query_key: str, url_query: Optional[Dict]
+) -> str:
+    param = os.environ.get(env_var_key, "")
+    if param == "" and url_query:
+        param = url_query.get(url_query_key, "")
+    if param == "":
+        logger.warning(
+            f"{env_var_key} not found in env vars and {url_query_key} not found in url query"
+        )
+    return param
--- tests/unit/clients/test_databricks_client.py
+++ tests/unit/clients/test_databricks_client.py
@@ -1,9 +1,33 @@
-from tentaclio.clients.databricks_client import build_odbc_connection_string
+import pytest
+from tentaclio.clients.databricks_client import build_odbc_connection_string, get_param_from_url_query_or_env_var
 
 
 def test_build_odbc_connection_string():
     output = build_odbc_connection_string(DRIVER="simba",
-                                           PORT=443,
-                                           UID="token")
+                                          PORT=443,
+                                          UID="token")
     expected = "DRIVER%3Dsimba%3BPORT%3D443%3BUID%3Dtoken"
     assert output == expected
+
+
+@pytest.mark.parametrize("env_value, expected", [
+    ("env_value", "env_value"),
+    ("", "url_value"),
+])
+def test_get_param_from_url_query_or_env_var(monkeypatch, env_value, expected):
+    monkeypatch.setenv("TENTACLIO__KEY", env_value)
+    url_query = {"url_key": "url_value"}
+    output = get_param_from_url_query_or_env_var("TENTACLIO__KEY", "url_key", url_query)
+    assert output == expected
+
+
+def test_get_param_from_url_query_or_env_var_when_no_env_var():
+    url_query = {"url_key": "url_value"}
+    output = get_param_from_url_query_or_env_var("TENTACLIO__KEY", "url_key", url_query)
+    assert output == "url_value"
+
+
+def test_get_param_from_url_query_or_env_var_when_no_env_var_or_url_value(caplog):
+    output = get_param_from_url_query_or_env_var("TENTACLIO__KEY", "url_key", dict())
+    assert output == ""
+    assert "TENTACLIO__KEY not found in env vars and url_key not found in url query" in caplog.text
